19a9d630925af98ac12181ffb23a1861ebeb3f8e
Harry Dodson <harry.lee.dodson@gmail.com>
2023-09-30 07:21:36 -0400

    updated user event. added group event.

--- events/events.go
+++ events/events.go
@@ -1,7 +1,19 @@
 package events
 
 type UserDataEvent struct {
-	NodeId   string `json:"nodeId"`
-	UserId   string `json:"userId"`
-	Username string `json:"username"`
+	NodeId         string `json:"nodeId"`
+	UserId         string `json:"userId"`
+	Username       string `json:"username"`
+	Status         string `json:"status"`
+	Comment        string `json:"comment"`
+	ReceiveUpdates bool   `json:"receiveUpdates"`
+}
+
+type GroupDataEvent struct {
+	Name             string `json:"name"`
+	Code             string `json:"code"`
+	GroupId          string `json:"groupId"`
+	OwnerId          string `json:"ownerId"`
+	KnownLanguage    string `json:"knownLanguage"`
+	LearningLanguage string `json:"learningLanguage"`
 }
